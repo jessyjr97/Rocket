a0196e04220dd508f6e51ee7318f6eacdaf98e9f
Jessy Rodrigue <rodrigue_jes@hotmail.com>
2018-08-24 17:04:49 -0400

    Ajouté la fonction de tourner

--- Rocket/Assets/Scripts/Rocket.cs
+++ Rocket/Assets/Scripts/Rocket.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Rocket : MonoBehaviour {
-    [SerializeField] public float ForceUpward = 2000;
+    [SerializeField] public float ForceUpward = 100;
+    [SerializeField] public float ForceTurning = 50;
     private Rigidbody rocketBody;
     // Use this for initialization
     void Start() {
@@ -16,5 +17,12 @@ public class Rocket : MonoBehaviour {
         {
             rocketBody.AddForce(new Vector3(0, ForceUpward, 0));
         }
-	}
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            rocketBody.AddForceAtPosition(new Vector3(0, ForceTurning, 0), new Vector3(0,0,0));
+        } else if (Input.GetAxis("Horizontal") < 0)
+        {
+            rocketBody.AddForceAtPosition(new Vector3(0, -ForceTurning, 0), new Vector3(0, 0, 0));
+        }
+    }
 }
