8045cc2ddaaf4b85bd3be6dfdca5d0b60dd9cdd5
Jessy Rodrigue <rodrigue_jes@hotmail.com>
2018-08-24 16:27:52 -0400

    Ajouté le Script pour monter

---
+++ Rocket/Assets/Scripts/Rocket.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+    [SerializeField] public float ForceUpward = 2000;
+    private Rigidbody rocketBody;
+    // Use this for initialization
+    void Start() {
+        rocketBody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetAxis("Jump") > 0)
+        {
+            rocketBody.AddForce(new Vector3(0, ForceUpward, 0));
+        }
+	}
+}
