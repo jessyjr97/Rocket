f68d6d11ed1a7a5ab6ae1bbf1ee1e757361560a5
Jessy Rodrigue <rodrigue_jes@hotmail.com>
2018-08-27 14:52:34 -0400

    Corrigé la rotation

--- Rocket/Assets/Scripts/Rocket.cs
+++ Rocket/Assets/Scripts/Rocket.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Rocket : MonoBehaviour {
-    [SerializeField] public float ForceUpward = 100;
-    [SerializeField] public float ForceTurning = 50;
+    [SerializeField] public float ForceUpward = 50;
+    [SerializeField] public float ForceTurning = 0.05f;
+    public float rocketNose = 2.35f;
     private Rigidbody rocketBody;
     // Use this for initialization
     void Start() {
@@ -15,14 +16,14 @@ public class Rocket : MonoBehaviour {
 	void Update () {
 		if (Input.GetAxis("Jump") > 0)
         {
-            rocketBody.AddForce(new Vector3(0, ForceUpward, 0));
+            rocketBody.AddRelativeForce(new Vector3(0, ForceUpward, 0), ForceMode.Force);
         }
         if (Input.GetAxis("Horizontal") > 0)
         {
-            rocketBody.AddForceAtPosition(new Vector3(0, ForceTurning, 0), new Vector3(0,0,0));
+            rocketBody.AddForceAtPosition(new Vector3(ForceTurning, 0, 0), new Vector3(0, rocketNose, 0));
         } else if (Input.GetAxis("Horizontal") < 0)
         {
-            rocketBody.AddForceAtPosition(new Vector3(0, -ForceTurning, 0), new Vector3(0, 0, 0));
+            rocketBody.AddForceAtPosition(new Vector3(-ForceTurning, 0, 0), new Vector3(0, rocketNose, 0));
         }
     }
 }
